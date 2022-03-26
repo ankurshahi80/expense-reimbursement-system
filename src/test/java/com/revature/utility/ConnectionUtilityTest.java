bf225f50fd3a5d687da400843cb261fb9da1140e
Ankur Shahi <shahiankur80@gmail.com>
2022-03-26 18:17:52 -0400

    Connected to the local Postgres database and pass database connection test

--- src/test/java/com/revature/utility/ConnectionUtilityTest.java
+++ src/test/java/com/revature/utility/ConnectionUtilityTest.java
@@ -2,10 +2,12 @@ package com.revature.utility;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 public class ConnectionUtilityTest {
 
     @Test
-    public void test_getConnection(){
+    public void test_getConnection() throws SQLException {
         ConnectionUtility.getConnection();
     }
 }
