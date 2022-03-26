4fef54422a580de34a38688adbc660034e5d1d2d
Ankur Shahi <shahiankur80@gmail.com>
2022-03-26 15:38:27 -0400

    Database connection test - fail

---
+++ src/test/java/com/revature/utility/ConnectionUtilityTest.java
@@ -0,0 +1,11 @@
+package com.revature.utility;
+
+import org.junit.jupiter.api.Test;
+
+public class ConnectionUtilityTest {
+
+    @Test
+    public void test_getConnection(){
+        ConnectionUtility.getConnection();
+    }
+}
